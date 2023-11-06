361a5c404821924f74a15c52fab15d7421431567
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-11-06 14:13:54 +0600

    fotter added

--- src/Components/Home/Feature.jsx
+++ src/Components/Home/Feature.jsx
@@ -3,7 +3,9 @@ import { Link } from 'react-router-dom';
 
 const Feature = () => {
     return (
-        <div className='w-11/12 mx-auto my-8 grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+        <div className='w-11/12 mx-auto my-10 '>
+            <h4 className='text-center font-semibold text-xl text-[#009fe2] mb-5'>Features</h4>
+            <div className='grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
             <div className="shadow-lg rounded-lg p-5 space-y-3 text-center">
                 <MdOutlineCreate className='text-5xl mx-auto'></MdOutlineCreate>
                 <h3 className="text-2xl md:text-3xl font-semibold">Create Assignment</h3>
@@ -30,6 +32,7 @@ const Feature = () => {
                     <Link to={"/manage-assignment"} className='text-xl py-2 px-6 text-white bg-[#009fe2] rounded-lg'>Manage</Link>
                 </div>
             </div>
+        </div>
         </div>
     );
 };
