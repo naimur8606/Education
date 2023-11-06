3de5f6f5cb3c709ef93d7be0388e1f271b55342e
naimur8606 <119111473+naimur8606@users.noreply.github.com>
2023-11-06 11:59:39 +0600

    banner and feature section added

--- src/Components/Home/Feature.jsx
+++ src/Components/Home/Feature.jsx
@@ -1,32 +1,34 @@
-import { GiUpgrade } from 'react-icons/gi';
-import { GrDocumentUpload } from 'react-icons/gr';
-import { MdManageHistory } from 'react-icons/md';
+import { MdAddTask, MdOutlineAssignment, MdOutlineCreate } from 'react-icons/md';
+import { Link } from 'react-router-dom';
 
 const Feature = () => {
     return (
-        <div className='w-11/12 mx-auto my-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-            <div className="feature-card">
-                <div className="feature-icon">
-                    <GiUpgrade></GiUpgrade>
+        <div className='w-11/12 mx-auto my-8 grid gap-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+            <div className="shadow-lg rounded-lg p-5 space-y-3 text-center">
+                <MdOutlineCreate className='text-5xl mx-auto'></MdOutlineCreate>
+                <h3 className="text-2xl md:text-3xl font-semibold">Create Assignment</h3>
+                <p className="text-base">Easily create assignments for your students or peers and manage them efficiently.</p>
+                <div className=''>
+                    <Link to={"/create-assignment"} className='text-xl py-2 px-6 text-white bg-[#009fe2] rounded-lg'>Create</Link>
                 </div>
-                <h3 className="feature-title">Grading and Feedback</h3>
-                <p className="feature-description">Effortlessly grade assignments and provide constructive feedback to enhance learning outcomes.</p>
             </div>
 
-            <div className="feature-card">
-                <div className="feature-icon">
-                <GrDocumentUpload></GrDocumentUpload>
+            <div className="shadow-lg rounded-lg p-5 space-y-3 text-center">
+                <MdOutlineAssignment className='text-5xl mx-auto'></MdOutlineAssignment>
+                <h3 className="text-2xl md:text-3xl font-semibold">My Assignments</h3>
+                <p className="feature-description">View and manage the assignments you have created, track their status, and monitor progress.</p>
+                <div>
+                    <Link to={"/my-assignment"} className='text-xl py-2 px-6 text-white bg-[#009fe2] rounded-lg'>View</Link>
                 </div>
-                <h3 className="feature-title">Upload and Share Assignments</h3>
-                <p className="feature-description">Easily upload assignments and collaborate with peers by sharing your work securely.</p>
             </div>
 
-            <div className="feature-card">
-                <div className="feature-icon">
-                <MdManageHistory></MdManageHistory>
+            <div className="shadow-lg rounded-lg p-5 space-y-3 text-center">
+                <MdAddTask className='text-5xl mx-auto'></MdAddTask>
+                <h3 className="text-2xl md:text-3xl font-semibold">Submitted Assignments</h3>
+                <p className="feature-description">Access and review assignments submitted by users, provide feedback, and track completion status.</p>
+                <div>
+                    <Link to={"/manage-assignment"} className='text-xl py-2 px-6 text-white bg-[#009fe2] rounded-lg'>Manage</Link>
                 </div>
-                <h3 className="feature-title">Assignment Management</h3>
-                <p className="feature-description">Efficiently create, update, and organize your assignments to stay on top of your coursework.</p>
             </div>
         </div>
     );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,15 +6,6 @@ import { useState } from "react";
 const Navbar = () => {
     const [menu, setMenu] = useState(false)
 
-    const assignmentItems = [
-        { name: "Assignment", link: "/assignment" },
-        { name: "Create Assignment", link: "/create-assignment" },
-        { name: "My Assignments", link: "/my-assignments" },
-        { name: "Submitted Assignments", link: "/submitted-assignments" },
-    ]
-
-
-
     return (
         <div className="w-11/12 mx-auto">
             <div className="absolute z-[1] py-5 w-11/12 flex justify-between items-center flex-row-reverse lg:flex-row">
@@ -34,26 +25,16 @@ const Navbar = () => {
                         }>
                         Home
                     </NavLink>
-                    <div className="dropdown">
-                        <label tabIndex={0} className="">Assignment</label>
-                        {
-                            <ul tabIndex={0} className="dropdown-content text-black z-[1] menu p-5 shadow bg-base-100 text-base rounded-box space-y-5 w-56">
-                                {
-                                    assignmentItems.map((assignmentItem, idx) =>
-                                        <NavLink
-                                            key={idx}
-                                            onClick={() => setMenu(false)}
-                                            to={assignmentItem.link}
-                                            className={({ isActive, isPending }) =>
-                                                isPending ? "pending" : isActive ? "border-b-2 border-[#fff] md:border-[#009fe2] px-2 rounded-md" : ""
-                                            }>
-                                            {assignmentItem.name}
-                                        </NavLink>
-                                    )
-                                }
-                            </ul>
-                        }
-                    </div>
+
+                    <NavLink
+                        onClick={() => setMenu(false)}
+                        to={"/assignments"}
+                        className={({ isActive, isPending }) =>
+                            isPending ? "pending" : isActive ? "border-b-2 border-[#fff] md:border-[#009fe2] px-2 rounded-md" : ""
+                        }>
+                        Assignments
+                    </NavLink>
+
                     <NavLink
                         to={"/login"}
                         className={({ isActive, isPending }) =>
